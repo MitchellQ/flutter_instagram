2b4da6c9c78915ca0e3b06bac8984c837b96cc5d
MitchellQ <mitchell.quarrie@outlook.com>
2022-04-15 21:38:30 +1200

    Update login_screen.dart
    Add sign in functionality

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram/utils/utils.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../resources/auth_methods.dart';
 import '../utils/colors.dart';
 import '../utils/screen.dart';
 import '../widgets/text_field_input.dart';
@@ -15,12 +17,33 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
 
   @override
   void dispose() {
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+  }
+
+  void loginUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    String res = await AuthMethods().loginUser(
+      email: _emailController.text,
+      password: _passwordController.text,
+    );
+
+    if (res != 'Login successful') {
+      showSnackBar(res, context);
+
+      return;
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
@@ -69,9 +92,20 @@ class _LoginScreenState extends State<LoginScreen> {
 
               //Sign in button
               InkWell(
-                onTap: () {},
+                onTap: () => loginUser(),
                 child: Container(
-                  child: const Text('Log in'),
+                  height: 48,
+                  child: _isLoading
+                      ? const Center(
+                          child: SizedBox(
+                            height: 35,
+                            width: 35,
+                            child: CircularProgressIndicator(
+                              color: primaryColor,
+                            ),
+                          ),
+                        )
+                      : const Text('Log in'),
                   width: width,
                   alignment: Alignment.center,
                   padding: const EdgeInsets.symmetric(vertical: 12),
